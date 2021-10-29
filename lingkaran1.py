457ae4491c56b3f184bf9b4a13e557d95e864820
agung tirta yasha <agungtirta599@gmail.com>
2021-10-29 19:18:42 +0700

    menmbahkan file lingkaran1.py

---
+++ lingkaran1.py
@@ -0,0 +1,15 @@
+import math
+r = float(input("Masukan Jari-jari : "))
+print("")
+
+luas = math.pi*(r*r)
+keliling = 2*math.pi*r
+
+print ("Luas Lingkaran \t\t= ",luas)
+print ("Keliling Lingkaran\t= ",keliling)
+print("")
+print (" -------nilai jari-jari (r)--------")
+print("")
+print ("Luas Lingkaran \t= ",format(luas,'.2f'))
+print ("Keliling Lingkaran \t= ",format(keliling,'.2f'))
+
